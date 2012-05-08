d3606bf912544a819f2dfb8b2c4f17ac14dd515b
awonderland6@googlemail.com <awonderland6@googlemail.com@4c589bc5-3898-6224-771f-302820b144f8>
2012-05-08 16:51:33 +0000

    * added ExtensionPoint and ExtensionPointTemplate  * fixed bug within XMLIteratorFactory  * added OperationVoid interface

---
+++ utils-apl-derived/utils-apl-derived-spring/src/main/java/org/omnaest/utils/spring/extension/ExtensionPointTemplate.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Copyright 2012 Danny Kunz
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.omnaest.utils.spring.extension;
+
+import java.util.List;
+
+import org.omnaest.utils.events.exception.ExceptionHandler;
+import org.omnaest.utils.operation.special.OperationVoid;
+
+/**
+ * Generic template implementation for an {@link ExtensionPoint} which allows to {@link #setExceptionHandler(ExceptionHandler)}
+ * 
+ * @see ExtensionPoint
+ * @param <S>
+ *          type of service
+ * @author Omnaest
+ */
+@ExtensionPoint
+public abstract class ExtensionPointTemplate<S>
+{
+  /* ********************************************** Beans / Services / References ********************************************** */
+  private final List<S>    beanList;
+  private ExceptionHandler exceptionHandler = null;
+  
+  /* ********************************************** Methods ********************************************** */
+  
+  /**
+   * @see ExtensionPointTemplate
+   * @param serviceList
+   */
+  protected ExtensionPointTemplate( List<S> serviceList )
+  {
+    super();
+    this.beanList = serviceList;
+  }
+  
+  /**
+   * Executes the given {@link OperationVoid} on all internal bean instances of this {@link ExtensionPointTemplate}
+   * 
+   * @param operation
+   */
+  protected void executeOnAll( OperationVoid<S> operation )
+  {
+    //
+    if ( operation != null && this.beanList != null )
+    {
+      for ( S bean : this.beanList )
+      {
+        if ( bean != null )
+        {
+          try
+          {
+            //
+            operation.execute( bean );
+          }
+          catch ( Exception e )
+          {
+            if ( this.exceptionHandler != null )
+            {
+              this.exceptionHandler.handleException( e );
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  /**
+   * @param exceptionHandler
+   *          {@link ExceptionHandler}
+   * @return this
+   */
+  public ExtensionPointTemplate<S> setExceptionHandler( ExceptionHandler exceptionHandler )
+  {
+    this.exceptionHandler = exceptionHandler;
+    return this;
+  }
+}
